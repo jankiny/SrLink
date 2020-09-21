69ef44a05ee0dbb0ba12b9ffeb0dfb447e12898e
Jankiny <yangzl99@Live.com>
2020-09-22 00:08:34 +0800

    Update Global.cs
    修改版本号

--- SRLink/SRLink/Global.cs
+++ SRLink/SRLink/Global.cs
@@ -7,7 +7,7 @@
         /// info
         /// </summary>
         public const string SoftwareName = "SRLink";
-        public const string Version = "v2.1.0_200804_alpha";
+        public const string Version = "v2.2.0_200922_beta";
 
         /// <summary>
         /// 校园认证地址
